363aa24224b662ba07799d8ceddf2572817dff90
Dan <43572775+profdambledore@users.noreply.github.com>
2019-03-26 11:34:03 +0000

    Further updates to pickup code
    Updated pickup code
     - Has an additional sprite attached to pickups - hitbox
        - This hitbox is currently visible, will need to be changed to invisible
     - Added pointerdown listener to all pickups, only over pickups
    
    Need to do
     - Continue on overlap code, currently doesn't work

---
+++ js/main.js
@@ -0,0 +1,341 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1152,
+    height: 1152,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                y: 0
+            }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+gtag('event', 'GameStart', {
+		'event_category': 'Iteration One',
+		'event_label': 'Game',
+		'value': "Started"
+	});
+
+// Global Important Variables
+var player, bullets, weaponCtrl;
+var drops = [];
+var enemies = [];
+var target;
+var round = 1;
+var score = 0;
+var updated = false;
+
+
+function preload() {
+    // Load Tileset
+    this.load.image('backTileset', 'assets/background.png')
+    this.load.tilemapTiledJSON('backTilemap', 'assets/tileset.json')
+
+    // Load Image Assets - Player
+    this.load.image('player', 'assets/player.png')
+    this.load.image('playerBullet', 'assets/playerBullet.png')
+	this.load.image('hitbox', 'assets/hitbox.png');
+	this.load.image('health_pickup', 'assets/health.png')
+	this.load.image('speed_pickup', 'assets/speedBoost.png');
+	this.load.image('shotgun_pickup', 'assets/shotgun.png');
+
+    // Load Image Assets - Enemy
+	// Level 1
+    this.load.image('enemy1', 'assets/enemy1.png')
+	
+	// Level 2
+	this.load.image('enemy2', 'assets/enemy2.png')
+	this.load.image('enemy2Bullet', 'assets/enemy2Bullet.png');
+}
+
+function create() {
+	// Create weaponCtrl
+	weaponCtrl = new WeaponCtrl();
+	
+    // Create Map from JSON
+    this.map = this.make.tilemap({ key: 'backTilemap' });
+    var tileset = this.map.addTilesetImage('background', 'backTileset')
+
+    // Create Layers from Tiled information
+    this.map.createStaticLayer('botGlowLayer', tileset, 0, 0);
+    var mainLayer = this.map.createStaticLayer('midGlowLayer', tileset, 0, 0);
+    this.map.createStaticLayer('topGlowLayer', tileset, 0, 0);
+
+    // Add collision to the main layer
+    mainLayer.setCollisionByProperty({ collide: true });
+
+    // Set max bounds for camera and player movement
+    this.cameras.main.setBounds(0, 0, 1152, 1152);
+    this.physics.world.setBounds(0, 0, 1152, 1152);
+
+    // Create the player object
+    player = new Player(this, game.config.width * 0.3, game.config.height * 0.5, 'player')
+    player.enableCollision(mainLayer);
+    //this.cameras.main.startFollow(player.hull, true, 0.5, 0.5);
+
+    // Create base bullet group for the player
+    pistolBullets = this.physics.add.group({
+        defaultKey: 'playerBullet',
+        maxSize: 5
+    });
+    this.input.on('pointerdown', tryShoot, this);
+
+    this.physics.world.on('worldbounds', function (body) {
+        killBullet(body.gameObject);
+    }, this);
+
+    createEnemy.call(this, 0, player, this.map);
+	
+	this.roundText = this.add.text(0, 0, 'Round = ' + round, {fontFamily: "Courier New"});
+	this.roundText.setFontSize(60);
+	
+	this.scoreText = this.add.text(750, 0, 'Score = ' + score, {fontFamily: "Courier New"});
+	this.scoreText.setFontSize(60);
+	
+	this.healthText = this.add.text(0, 1092, 'Health = ' + player.currentHealth, {fontFamily: "Courier New"});
+	this.healthText.setFontSize(60);
+	
+	this.levelText = this.add.text(750, 1092, 'SPD = ' + player.spdLvl, {fontFamily: "Courier New"});
+	this.levelText.setFontSize(60);
+}
+
+function update(time, delta) {
+    // Allow the player class to update
+    player.update(time, delta);
+	this.scoreText.setText('Score = ' + score);
+	this.healthText.setText('Health = ' + player.currentHealth);
+	
+	// Set Speed Text
+	// Google Analytics Tracking
+	if (player.spdLvl == 10 && updated == false) {
+		gtag('event', 'HitMaxSpeed', {
+			'event_category': 'Iteration One',
+			'event_label': 'Time',
+			'value': time
+		});
+		this.levelText.setText('SPD = MAX');
+		updated = true;
+	}
+	else {
+		this.levelText.setText('SPD = ' + player.spdLvl);
+	}
+
+    // Allow any enemies in the array to update
+    for (var i = 0; i < enemies.length; i++) {
+        enemies[i].update(time, delta);
+    }
+
+    if (enemies.length < 1) {
+        round++;
+        createEnemy.call(this, 0, player, this.map);
+		this.roundText.setText('Round = ' + round);
+    }
+	
+	// Add tap listeners to all pickups in the array, based on how close they are
+	for (var i = 0; i < drops.length; i++) {
+		}
+}
+
+function createEnemy(level, player, map) {
+	// Choose which spawn to choose the enemy at
+	var randSpawn = Math.floor(Math.random() * Math.floor(4));
+	var enemySpawn = map.findObject("spawn", function (object)
+		{
+			if (object.id === (randSpawn + 9)) {
+				return object;
+			}
+		});
+	switch (level) {
+        case 0:
+            enemy = new Level0Enemy(this, 576, 576, player);
+            enemies.push(enemy);
+            this.physics.add.overlap(player.hull, enemy.hull, meleeDamage, null, this);
+            break;
+		
+		case 1:
+			enemy = new Level1Enemy(this, enemySpawn.x, enemySpawn.y, player);
+			enemies.push(enemy);
+			this.physics.add.overlap(player.hull, enemy.hull, meleeDamage, null, this);
+			break;
+	
+			
+		case 2:
+			enemy = new Level2Enemy(this, enemySpawn.x, enemySpawn.y, player);
+			enemies.push(enemy);
+			this.physics.add.overlap(player.hull, enemy.hull, meleeDamage, null, this);
+			break;
+	}
+}	
+
+function createRound(player, map) {
+	gtag('event', 'RoundStart', {
+		'event_category': 'Iteration One',
+		'event_label': 'Rounds',
+		'value': round
+	});
+    var totalSpawn = (Math.floor(round * 1.5)) + 5;
+	gtag('event', 'AmountEnemies', {
+		'event_category': 'Iteration One',
+		'event_label': 'Enemies',
+		'value': totalSpawn
+	});
+    console.log("Round Enemies = " + totalSpawn)
+    for (var i = 0; i < totalSpawn; i++) {
+        var randLevel = Phaser.Math.Between(1, 2);
+        createEnemy.call(this, randLevel, player, map);
+
+        // Add colliders to each enemy already in the array
+        if (i > 0) {
+            for (var j = 0; j < enemies.length - 1; j++) {
+                this.physics.add.collider(enemy.hull, enemies[j].hull);
+            }
+        }
+    }
+
+}
+
+function tryShoot(pointer) {
+    var type = player.getType();
+    if (type == 1) {
+        var bullet = pistolBullets.get(player.hull.x, player.hull.y);
+        if (bullet) {
+            fireBullet.call(this, bullet, player.hull.rotation, enemies);
+        }
+    }
+    else if (type == 2) {
+        var bullet = shotgunBullets.get(player.hull.x, player.hull.y);
+        if (bullet) {
+            fireBullet.call(this, bullet, player.hull.rotation, enemies);
+        }
+    }
+}
+
+function fireBullet(bullet, rotation, target) {
+	if (target != player) {
+		var type = player.getType();
+        if (type === 1) {
+            weaponCtrl.weaponTypeOne(bullet, rotation);
+            this.physics.velocityFromRotation(bullet.rotation, 500, bullet.body.velocity);
+        }
+        else if (type === 2) {
+            for (var j = 0; j < 8; j++) {
+                weaponCtrl.weaponTypeTwo(bullet, rotation);
+                this.physics.velocityFromRotation(bullet.rotation, 200, bullet.body.velocity);
+            }
+        }
+    }
+	else {
+		weaponCtrl.enemyLevel2(bullet, rotation);
+		this.physics.velocityFromRotation(bullet.rotation, 500, bullet.body.velocity);
+	}
+
+    if (target === player) {
+        this.physics.add.overlap(player.hull, bullet, bulletHitPlayer, null, this);
+    }
+    else {
+        for (var i = 0; i < enemies.length; i++) {
+            this.physics.add.overlap(enemies[i].hull, bullet, bulletHitEnemy, null, this);
+        }
+    }
+}
+
+function killBullet(bullet) {
+    bullet.disableBody(true, true);
+    bullet.setActive(false);
+    bullet.setVisible(false);
+}
+
+function bulletHitPlayer(hull, bullet) {
+	killBullet(bullet);
+	var damageType = 'bullet'
+	player.damage(damageType)
+	if (player.isDestroyed() == true) {
+		this.physics.pause();
+		console.log("You Lose");
+	}
+}
+
+function bulletHitEnemy(hull, bullet) {
+    var enemy;
+    var index;
+    for (var i = 0; i < enemies.length; i++) {
+        if (enemies[i].hull === hull) {
+            enemy = enemies[i];
+            index = i;
+            break;
+        }
+    }
+    killBullet(bullet);
+    enemy.damage();
+
+    if (enemy.alive == false) {
+        if (enemies[i].level == 0) {
+            createRound.call(this, player, this.map)
+			score = score - 1;
+        }
+		createDrop.call(this, enemy.level, enemy.hull.x, enemy.hull.y)
+        enemies.splice(index, 1);
+		score = score + 1;
+    }
+}
+
+function meleeDamage() {
+	// If a tank overlaps with the player, this functionis called
+	var damageType = 'melee';
+	player.damage(damageType);
+	if (player.isDestroyed() == true) {
+		this.physics.pause();
+		console.log("You Lose");
+	}
+}
+
+function createDrop(level, locX, locY) {
+	var spawnChance = Phaser.Math.Between(0, 100);
+	if (level == 0) {
+		return;
+	}
+	else {
+		if (spawnChance <= 10) {
+			speedDrop = new SpeedPowerup(this, locX, locY, player);
+			speedDrop.hull.setInteractive();
+			drops.push(speedDrop);
+		}
+		else if (spawnChance >= 95) {
+			healthDrop = new HealthPowerup(this, locX, locY, player);
+			healthDrop.hull.setInteractive();
+			drops.push(healthDrop);
+		}
+	}
+}
+
+function useDrop(hull, playerHull) {
+	var drop;
+	var index;
+	for (var i = 0; i < drops.length; i++) {
+        if (drops[i].hull === hull) {
+            drop = drops[i];
+            index = i;
+            break;
+        }
+    }
+	if (drop.dropType == 'speed') {	
+		player.setMaxSpeed(10);
+		drop.collected();
+		drops.splice(index, 1);
+	}
+	else if (drop.dropType == 'health') {	
+		player.setHealth(20);
+		drop.collected();
+		drops.splice(index, 1);
+	}
+	drop.hull.remove('pointerdown', clickPickupDown)
+}
---
+++ js/player.js
@@ -0,0 +1,199 @@
+class Player {
+    constructor(scene, x, y, texture) {
+        // Set up properties
+        this.scene = scene;
+        this.currentHealth = 100;
+        this.currentSpeed = 0;
+		this.maxSpeed = 250;
+        this.takenDamage = false;
+        this.invunTime = 0;
+        this.hull = scene.physics.add.sprite(x, y, texture);
+        this.hull.body.setSize(this.hull.width - 8, this.hull.height - 8);
+        this.hull.body.colideWorldBounds = true;
+        this.weaponType = 1;
+		
+		// Properties for levels
+		this.spdLvl = 0;
+
+        // Add listeners for mobile devices
+        this.scene.input.on('pointerdown', this.handlePointerDown, this);
+        this.scene.input.on('pointerup', this.handlePointerUp, this);
+        this.touchData = {};
+    }
+
+    update(time, delta) {
+        // Update player movement
+        const worldPoint = this.scene.input.activePointer.positionToCamera(this.scene.cameras.main);
+        this.hull.rotation = Phaser.Math.Angle.Between(this.hull.x, this.hull.y, worldPoint.x, worldPoint.y);
+        this.scene.physics.velocityFromRotation(this.hull.rotation, this.currentSpeed, this.hull.body.velocity);
+		
+		//Check invunrability
+		if (this.takenDamage == true && this.invunTime == 0) {
+			this.invunTime = time;
+			console.log('made invun');
+		}
+		if (this.invunTime > 0) {
+			if (time > this.invunTime + 2000) {
+				this.invunTime = 0;
+				this.takenDamage = false;
+				console.log('no longer invun');
+			}
+		}
+    }
+
+    handlePointerDown(pointer) {
+        this.touchData.startX = pointer.x;
+        this.touchData.startY = pointer.y;
+        this.currentSpeed = this.maxSpeed;
+    }
+
+    handlePointerUp(pointer) {
+        this.touchData.endX = pointer.x;
+        this.touchData.endY = pointer.y;
+        this.currentSpeed = 0;
+        this.handleTouch();
+    }
+
+    handleTouch() {
+        const distX = this.touchData.endX - this.touchData.startX;
+        const distY = this.touchData.endY - this.touchData.startY;
+        this.touchData = {};
+    }
+
+    enableCollision(mainLayer) {
+        // Enable collision between the player and the border
+        this.scene.physics.add.collider(this.hull, mainLayer);
+    }
+	
+	setType(weapon) {
+		this.weaponType = weapon;
+	}
+	
+	getType(){
+		// Returns what weapon type is currently selected
+		return this.weaponType;
+	}
+	
+	setHealth(amount){
+		this.currentHealth = this.currentHealth + amount;
+		if (player.currentHealth > 100) {
+			player.currentHealth = 100;
+		}
+	}
+	
+	setMaxSpeed(amount) {
+		this.maxSpeed = this.maxSpeed + amount;
+		this.spdLvl++;
+		if (this.spdLvl == 10) {
+			this.maxSpeed = 350
+			this.spdLvl = 10
+		}
+	}
+	
+	damage(damageType) {
+		if (this.takenDamage == false) {	
+			if (damageType === 'melee') {
+				this.currentHealth = this.currentHealth - 5;
+				console.log(this.currentHealth);
+				this.scene.cameras.main.shake(200,0.005);
+				this.takenDamage = true;
+			}
+			else if (damageType === 'bullet') {
+				this.currentHealth = this.currentHealth - 10;
+				console.log(this.currentHealth);
+				this.scene.cameras.main.shake(200,0.01);
+				this.takenDamage = true;
+			}
+		}
+	}
+	
+	isDestroyed() {
+		// Test to see if the player is destroyed or not, when damage is taken
+		if (this.currentHealth <= 0) {
+			this.scene.input.off('pointerdown', this.handlePointerDown, this);
+			this.scene.input.off('pointerup', this.handlePointerUp, this);
+			gtag('event', 'GameEnd', {
+				'event_category': 'Iteration One',
+				'event_label': 'Score',
+				'value': this.scene.score
+			});
+			return true;
+		}
+	}
+}
+
+// Player Powerups
+
+class BasePowerup {
+	constructor(scene, x, y, texture, player) {
+		this.scene = scene;
+		this.player = player;
+		this.hull = this.scene.physics.add.sprite(x, y, texture);
+		this.hitBox = this.scene.physics.add.sprite(x, y, 'hitbox');
+		this.hitBox.body.setSize(200, 200);
+		this.hull.body.collideWorldBounds = true;
+		this.hull.setDepth(4);
+		this.hull.on('pointerdown', this.clickPickupDown, this);
+		this.check = this.scene.physics.add.overlap(this.player, this.hitBox)
+	}
+	
+	clickPickupDown(pointer) {
+		console.log(this.hitBox);
+		if (this.check == true) {
+			console.log("Hello")
+		}
+		console.log("Behind");
+	}
+	
+	collected() {
+		this.hull.destroy();
+	}
+}
+
+class HealthPowerup extends BasePowerup {
+	constructor(scene, x, y, player) {
+		var texture = 'health_pickup';
+		super(scene, x, y, texture, player);
+		this.dropType = 'health';
+		this.hull.body.setSize(this.hull.width - 16, this.hull.height - 16);
+	}
+	
+	clickPickupDown(pointer) {
+		super.clickPickupDown(pointer);
+	}
+	
+	collected() {
+		super.collected();
+	}
+}
+
+class SpeedPowerup extends BasePowerup {
+	constructor(scene, x, y, player) {
+		var texture = 'speed_pickup';
+		super(scene, x, y, texture, player);
+		this.dropType = 'speed';
+		this.hull.body.setSize(this.hull.width - 16, this.hull.height - 16);
+	}
+	
+	clickPickupDown(pointer) {
+		super.clickPickupDown(pointer);
+	}
+	
+	collected() {
+		super.collected();
+	}
+}
+
+class ShotgunPickup extends BasePowerup {
+	constructor(scene, x, y, player) {
+		var texture = 'shotgun_pickup';
+		super(scene, x, y, texture, player);
+		this.dropType = 'weapon_shotgun';
+		this.hull.body.setSize(this.hull.width - 16, this.hull.height - 16);
+	}
+	
+	collected() {
+		super.collected();
+		player.setType(2);
+	}
+}	
